2a06e36e85603b7350385485073b28bd7acf50af
Marco Bacci <marcobacci94@gmail.com>
2025-07-10 14:41:04 +0200

    feat: input  structure

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const UserAge = parseFloat(prompt ('inserisci età'))
+// const NumKm = parseFloat(prompt ('inserisci numero km'))
+// let TicketPrice = NumKm * 0.21
+
+// if (UserAge <= 18){
+//   TicketPrice = TicketPrice * 0.8
+// }
+
+
+// else if (UserAge >= 65){
+//   TicketPrice = TicketPrice * 0.6
+// }
+
+// console.log(`final price € ${TicketPrice.toFixed(2)}`)
