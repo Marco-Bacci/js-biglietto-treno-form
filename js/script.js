2de5e70c5d11cc750f44400e10eaa4c3855b4470
Marco Bacci <marcobacci94@gmail.com>
2025-07-10 16:24:07 +0200

    fix: back to basics

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-// recupero il button per inviare il form
-const button = document.getElementById(`send`)
-// creo l'evento click sul pulsante
-button.addEventListener (`click`, function(e){
+
+// Recupero valori del form
+const button = document.getElementById (`send`)
+button.addEventListener (`click`, function(e))  
+{
   e.preventDefault();
-  // Recupero valori del form
-  const name = document.getElementById (`name`).value;
-  const numKm = document.getElementById (`km`).value;
-  const userAge = document.getElementById (`eta`).value;
-  console.log(name, numKm, userAge)
-
-})
+  const km = document.getElementById (`km`).value
+  const age = document.getElementById (`age`).value
+  
+  console.log(km,age)
+}
 
 
+  
+  
+  
 
-
-
-
-
+  
 
 
 
+// variabile costo del biglietto
+  // let TicketPrice = numKm * 0.21
+  
+  // if (userAge <= 18){
+  //   TicketPrice = TicketPrice * 0.8
+  // }
+  
+  // else if (userAge >= 65){
+  //   TicketPrice = TicketPrice * 0.6
+  // }
+  
+  // console.log(numKm, userAge , ticketPrice.toFixed(2))
 
+// // recupero il button per inviare il form
+// const button = document.getElementById(`send`)
 
-
-
+// // creo l'evento click sul pulsante
+// button.addEventListener (`click`, function(e){
+//   e.preventDefault();
+// })
 
 
